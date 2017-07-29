cb70ad9b8d64fb82bfa29a87dc58657c14071348
Danny van Velzen <dannyvv@microsoft.com>
2017-07-28 19:18:49 -0700

    Add external analyzer support

---
+++ src/Codex.Analysis/External/CodexTypeAdapter.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Codex.ObjectModel;
+
+namespace Codex.Analysis.External
+{
+    public class CodexTypeAdapter
+    {
+        public static void Annotate(BoundSourceFileBuilder builder, CodexFile file)
+        {
+            builder.AddClassifications(FilterSpans(file.Spans.Select(span => ToClassification(file.Store, span)).Where(c => c != null)));
+
+            foreach (var reference in FilterSpans(file.Spans.Select(span => ToReference(file.Store, builder, span)).Where(c => c != null)))
+            {
+                builder.AnnotateReferences(reference.Start, reference.Length, reference.Reference);
+            }
+
+            foreach (var definition in FilterSpans(file.Spans.Select(span => ToDefinition(file.Store, builder, span)).Where(c => c != null)))
+            {
+                builder.AnnotateDefinition(definition.Start, definition.Length, definition.Definition);
+            }
+        }
+
+        public static IEnumerable<T> FilterSpans<T>(IEnumerable<T> spans)
+            where T : Span
+        {
+            int minStart = 0;
+            foreach (var span in spans)
+            {
+                if (span.Start >= minStart)
+                {
+                    minStart = span.Start + span.Length;
+                    yield return span;
+                }
+            }
+        }
+
+        public static ClassificationSpan ToClassification(CodexSemanticStore store, CodexSpan span)
+        {
+            if (!span.Classification.IsValid && !span.Symbol.IsValid)
+            {
+                return null;
+            }
+
+            return new ClassificationSpan()
+            {
+                Start = span.Start,
+                Length = span.Length,
+                LocalGroupId = span.LocalId.IsValid ? span.LocalId.Id + 1 : 0,
+                Classification = span.Classification.IsValid ? store.Classifications[span.Classification].Name : "text"
+            };
+        }
+
+        public static ReferenceSpan ToReference(CodexSemanticStore store, BoundSourceFileBuilder builder, CodexSpan span)
+        {
+            if (!span.ReferenceKind.IsValid || !span.Symbol.IsValid)
+            {
+                return null;
+            }
+
+            var symbol = store.Symbols[span.Symbol];
+            SymbolId symbolId = GetSpanSymbolId(symbol);
+
+            var projectId = symbol.Project.IsValid ? store.Projects.Get(symbol.Project).Name : builder.ProjectId;
+
+            return new ReferenceSpan()
+            {
+                Start = span.Start,
+                Length = span.Length,
+                Reference = new ReferenceSymbol()
+                {
+                    Id = symbolId,
+                    ReferenceKind = store.ReferenceKinds[span.ReferenceKind].Name,
+                    Kind = "symbol",
+                    ProjectId = projectId,
+                },
+            };
+        }
+
+        public static DefinitionSpan ToDefinition(CodexSemanticStore store, BoundSourceFileBuilder builder, CodexSpan span)
+        {
+            if ((span.ReferenceKind.IsValid && !string.Equals(nameof(ReferenceKind.Definition), store.ReferenceKinds[span.ReferenceKind].Name,
+                StringComparison.OrdinalIgnoreCase)) || !span.Symbol.IsValid)
+            {
+                return null;
+            }
+
+            var symbol = store.Symbols[span.Symbol];
+            SymbolId symbolId = GetSpanSymbolId(symbol);
+
+            var projectId = symbol.Project.IsValid ? store.Projects.Get(symbol.Project).Name : builder.ProjectId;
+
+            return new DefinitionSpan()
+            {
+                Start = span.Start,
+                Length = span.Length,
+                Definition = new DefinitionSymbol()
+                {
+                    Id = symbolId,
+                    ShortName = symbol.ShortName,
+                    DisplayName = symbol.DisplayName,
+                    ContainerQualifiedName = symbol.ContainerQualifiedName,
+                    SymbolDepth = symbol.SymbolDepth,
+                    ReferenceKind = nameof(ReferenceKind.Definition),
+                    Kind = symbol.Kind,
+                    ProjectId = projectId,
+                },
+            };
+        }
+
+        private static SymbolId GetSpanSymbolId(CodexSymbol symbol)
+        {
+            SymbolId symbolId;
+            if (symbol.ExtensionData == null)
+            {
+                symbolId = SymbolId.CreateFromId(symbol.UniqueId);
+                symbol.ExtensionData = symbolId;
+            }
+            else
+            {
+                symbolId = (SymbolId)symbol.ExtensionData;
+            }
+
+            return symbolId;
+        }
+    }
+
+}
---
+++ src/Codex.Analysis/External/ExternalRepoFileAnalyzer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Codex.Analysis.Files;
+using Codex.Import;
+
+namespace Codex.Analysis.External
+{
+    public class ExternalRepoFileAnalyzer : RepoFileAnalyzer
+    {
+        private readonly HashSet<string> semanticDataDirectories;
+
+        private static readonly string[] supportedExtensions = new[] {".dsc"};
+
+        public override string[] SupportedExtensions => supportedExtensions;
+
+        public Dictionary<string, CodexFile> m_fileToStoreMap =
+            new Dictionary<string, CodexFile>(StringComparer.OrdinalIgnoreCase);
+
+        public ExternalRepoFileAnalyzer(string[] semanticDataDirectories)
+        {
+            this.semanticDataDirectories = new HashSet<string>(semanticDataDirectories ?? new string[0],
+                StringComparer.OrdinalIgnoreCase);
+        }
+
+        public override void Initialize(Repo repo)
+        {
+            var dd = new Dictionary<string, RepoProject>();
+            foreach (var semanticDataDirectory in semanticDataDirectories)
+            {
+                var store = new CodexSemanticStore(semanticDataDirectory);
+                store.Load();
+
+                foreach (var project in store.Projects.List)
+                {
+                    var repoProject = new RepoProject(project.Name, repo)
+                    {
+                        ProjectDirectory = project.Directory,
+                        // $TODO;
+                        //ProjectFile = projectFile
+                    };
+
+                    dd.Add(project.Name, repoProject);
+
+                    repo.Projects.Add(repoProject);
+                }
+
+                foreach (var file in store.Files.List)
+                {
+                    if (!m_fileToStoreMap.ContainsKey(file.Path))
+                    {
+                        RepoFile repoFile;
+                        if (!repo.FilesByPath.TryGetValue(file.Path, out repoFile))
+                        {
+                            RepoProject project;
+                            if (!dd.TryGetValue(store.Projects[file.Project].Name, out project))
+                            {
+                                project = repo.DefaultRepoProject;
+                            }
+
+                            repoFile = project.AddFile(file.Path);
+                        }
+
+                        m_fileToStoreMap[repoFile.RepoRelativePath] = file;
+                    }
+                }
+            }
+        }
+
+        protected override void AnnotateFile(BoundSourceFileBuilder binder)
+        {
+            CodexFile file;
+            if (m_fileToStoreMap.TryGetValue(binder.SourceFile.Info.RepoRelativePath, out file))
+            {
+                file.Load();
+                CodexTypeAdapter.Annotate(binder, file);
+            }
+
+            base.AnnotateFile(binder);
+        }
+
+    }
+}
