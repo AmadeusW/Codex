971f0746c63718a8c01f4e8415ee1f428d17c8fb
Amadeusz Wieczorek <git@amadeusw.com>
2017-08-07 10:20:38 -0700

    Adds a controller

---
+++ src/Codex.API/Controllers/UploadController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Codex.API.Controllers
+{
+    public class UploadController : ApiController
+    {
+        // GET: api/Upload
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Upload/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Upload
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Upload/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Upload/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
